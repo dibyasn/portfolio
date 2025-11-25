f945410129afcf106a44cd05615e53435222b6ba
dibyasn <dibyacttc@gmail.com>
2025-11-25 23:25:54 +0530

    Fix deployment paths

--- src/components/MangroveModel.jsx
+++ src/components/MangroveModel.jsx
@@ -8,7 +8,7 @@ import { useGLTF } from '@react-three/drei'
 import { useEffect } from 'react'
 
 export default function MangroveModel() {
-    const { scene } = useGLTF('/model/stylized_mangrove_greenhouse/scene.gltf')
+    const { scene } = useGLTF('./model/stylized_mangrove_greenhouse/scene.gltf')
 
     useEffect(() => {
         if (scene) {
@@ -24,4 +24,4 @@ export default function MangroveModel() {
     return <primitive object={scene} scale={1} />
 }
 
-useGLTF.preload('/model/stylized_mangrove_greenhouse/scene.gltf')
+useGLTF.preload('./model/stylized_mangrove_greenhouse/scene.gltf')
